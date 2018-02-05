6ed24fa384fbc0d55c566b7a1263f0e6550f102f
Maximilien Rufin <maximilien.rufin@gmail.com>
2018-02-05 17:55:56 +0100

    first part of flats is done

--- config.rb
+++ config.rb
@@ -21,6 +21,6 @@ activate :deploy do |deploy|
   deploy.deploy_method = :git
 end
 
-# ["maxr7589", "jbdanchald", "monsieurpaillard"].each do |name|
-#   proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
-# end
+["maxr7589", "jbdanchald", "monsieurpaillard"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
